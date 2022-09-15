c13fcd97ced8725fea513f1fd0a3ef9e882f68b1
Jonathan <jonathan.hachez@gmail.com>
2022-09-15 14:24:27 +0200

    added new clock

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 
 print("Testing in Progress")
 from tkinter import Tk, Label
+from datetime import datetime
 
 window = Tk()
 window.title("Digital Clock")
@@ -9,4 +10,11 @@ window.geometry("600x300")
 window.configure(bg="steelblue")
 label = Label(window, font=("Arial Black",78,"bold"), bg="steelblue", fg="white")
 label.pack(pady=100)
+
+def clock():
+    time = datetime.now().strftime("%H:%M:%S")
+    label.configure(text=time)
+    label.after(500,clock)
+
+clock()
 window.mainloop()
